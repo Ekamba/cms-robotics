9d5537c618cf2ef5b95041d771d03d1b19840a4b
Axel Ekamba <axel.ekamba@cm-robotics.com>
2023-01-13 20:18:22 +0100

    starting working on the new design

--- src/components/category/prepare/Prepare.jsx
+++ src/components/category/prepare/Prepare.jsx
@@ -9,110 +9,55 @@ import {
   addWineSpritzToTrolley,
 } from "../../../features/trolleys/trolleySlice";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import types from "@testing-library/user-event";
 
 const Prepare = () => {
-  const [show, setShow] = useState();
+  const [show, setShow] = useState(false);
+  const [types, setTypes] = useState({
+    data: [
+      {
+        value: "Fresh Goods",
+      },
+      {
+        value: "Fruit & Veg",
+      },
+      {
+        value: "Deli",
+      },
+      {
+        value: "Frozen",
+      },
+
+      {
+        value: "Wine Spritz",
+      },
+    ],
+  });
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     document.title = "Prepare";
-  }, []);
+  });
 
   return (
     <div className="prepare__container">
       <div onClick={() => setShow(true)} className="prepare prepare__fresh">
         <img src="/assets/freshGoods.png" alt="fresh" />
         <p className="prepare__text">Fresh Goods</p>
-        {/* {show && (
-          <div className="modal">
-            <div className="inner__modal">
-              <RxCross2 onClick={() => setShow(false)} />
-              <p className="selected__text">You have selected</p>
-              <p className="item__name">*Prepare - Fresh Goods*</p>
-              <Link
-                onClick={() => dispatch(addFreshGoodsToTrolley())}
-                to="/trolleys"
-                className="confirm__btn"
-              >
-                Confirm
-              </Link>
-              <button onClick={() => setShow(false)} className="cancel__btn">
-                Cancel
-              </button>
-            </div>
-          </div>
-        )} */}
       </div>
       <div onClick={() => setShow(true)} className="prepare prepare__fruits">
         <img src="/assets/fruit_veg.png" alt="dry/good" />
         <p className="prepare__text">Fruit & Veg</p>
-        {/* {show && (
-          <div className="modal">
-            <div className="inner__modal">
-              <RxCross2 onClick={() => setShow(false)} />
-              <p className="selected__text">You have selected</p>
-              <p className="item__name">*Prepare - Fruit & Veg*</p>
-              <Link
-                onClick={() => dispatch(addFruitsVegToTrolley())}
-                to="/trolleys"
-                className="confirm__btn"
-              >
-                Confirm
-              </Link>
-              <button onClick={() => setShow(false)} className="cancel__btn">
-                Cancel
-              </button>
-            </div>
-          </div>
-        )} */}
       </div>
       <div onClick={() => setShow(true)} className="prepare prepare__deli">
         <img src="/assets/deli.png" alt="dry/good" />
         <p className="prepare__text">Deli</p>
-        {/* {show && (
-          <div className="modal">
-            <div className="inner__modal">
-              <RxCross2 onClick={() => setShow(false)} />
-              <p className="selected__text">You have selected</p>
-              <p className="item__name">*Prepare - Deli*</p>
-              <Link
-                onClick={() => dispatch(addDeliToTrolley())}
-                to="/trolleys"
-                className="confirm__btn"
-              >
-                Confirm
-              </Link>
-              <button onClick={() => setShow(false)} className="cancel__btn">
-                Cancel
-              </button>
-            </div>
-          </div>
-        )} */}
       </div>
       <div onClick={() => setShow(true)} className="prepare prepare__frozen">
         <img src="/assets/frozen.png" alt="dry/good" />
-        {/* <p className="prepare__text">Frozen</p>
-        {show && (
-          <div className="modal">
-            <div className="inner__modal">
-              <RxCross2 onClick={() => setShow(false)} />
-              <p className="selected__text">You have selected</p>
-              <p className="item__name">*Prepare - Frozen*</p>
-              <Link
-                onClick={() => dispatch(addFrozenToTrolley())}
-                to="/trolleys"
-                className="confirm__btn"
-              >
-                Confirm
-              </Link>
-              <button onClick={() => setShow(false)} className="cancel__btn">
-                Cancel
-              </button>
-            </div>
-          </div>
-        )} */}
+        <p className="prepare__frozen">Frozen</p>
       </div>
       <div
         onClick={() => setShow(true)}
@@ -121,17 +66,27 @@ const Prepare = () => {
         <img src="/assets/wine_spritz.png" alt="electronic" />
         <p className="prepare__text">Wine Spritz</p>
       </div>
-      <img
-        className="undraw"
-        src="/assets/undraw_gone_shopping_re_2lau 1.png"
-        alt=""
-      />
+      <div className="prepare prepare__undraw">
+        <img
+          className="undraw"
+          src="/assets/undraw_gone_shopping_re_2lau 1.png"
+          alt="undraw"
+        />
+      </div>
       {show && (
         <div className="modal">
           <div className="inner__modal">
             <RxCross2 onClick={() => setShow(false)} />
             <p className="selected__text">You have selected</p>
-            <p className="item__name">*Prepare - Wine Spritz*</p>
+            {types.data.map((type, i) =>
+              i === type.value ? (
+                <p key={i} className="item__name">
+                  * Prepare - {type.value} *
+                </p>
+              ) : (
+                ""
+              )
+            )}
             <Link
               onClick={() => dispatch(addWineSpritzToTrolley())}
               to="/trolleys"
@@ -150,3 +105,15 @@ const Prepare = () => {
 };
 
 export default Prepare;
+
+// {
+//   data.purpose?.map((item, index) =>
+//     index === 0 ? (
+//       <li key={index} onClick={() => handleSelect(item)}>
+//         {item?.value}
+//       </li>
+//     ) : (
+//       <li key={index}>{item?.value}</li>
+//     )
+//   );
+// }
